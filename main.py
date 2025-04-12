18311dcb05251964503b74069224310ec08a4a5e
unknown <oethsryem@gmail.com>
2025-04-11 23:54:07 -0400

    setup flask

---
+++ main.py
@@ -0,0 +1,16 @@
+# import numpy as np
+# import pandas as pd
+import os
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def get_timestamp():
+    now = datetime.datetime.now(datetime.timezone.utc)
+    timestamp = now.isoformat()
+    return timestamp, 200, {"Content-Type": "text/plain"}
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
