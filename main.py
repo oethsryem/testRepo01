5cfcd7755d47839c1011d83e3aab35a2909cc99e
unknown <oethsryem@gmail.com>
2025-04-12 21:16:08 -0400

    Allow fail messages

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ def get_timestamp():
     timestamp = now.isoformat()
     return timestamp, 200, {"Content-Type": "text/plain"}
 
+@app.route("/4")
+def get_timestamp():
+    now = datetime.datetime.now(datetime.timezone.utc)
+    timestamp = now.isoformat()
+    return timestamp, 400, {"Content-Type": "text/plain"}
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
